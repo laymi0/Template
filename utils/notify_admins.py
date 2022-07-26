e3b37dedd2237a073cd4353081870df50022610e
Азизбек <72821937+laymi0@users.noreply.github.com>
2022-07-26 11:46:00 +0500

    Update notify_admins.py

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -5,7 +5,6 @@ from aiogram import Bot, Dispatcher
 
 from data.config import ADMINS
 
-
 async def on_startup_notify(dp: Dispatcher, bot: Bot):
     
     me = await bot.get_me()
@@ -13,7 +12,6 @@ async def on_startup_notify(dp: Dispatcher, bot: Bot):
     for admin in ADMINS:
         try:
             
-
             await dp.bot.send_message(admin, f"<b>В {datetime.now().replace(microsecond=0)} Запущен бот {me.full_name}</b>\n\n"
             f'<b>Информация о боте:\nИмя:</b> <code>{me.full_name}</code>\n'
             f'<b>Юзернейм</b>: <code>{me.username}</code>\n'
